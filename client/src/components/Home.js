3bc3c20fe41e3cf2aa985f9cf6616d157ccac70f
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-06-10 14:51:14 -0600

    setup redirect from homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,27 @@
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom";
+
 const Home = () => {
+    const [redirect, setRedirect] = useState(false);
     const setUsernameSumbit=(e)=>{
         e.preventDefault();
         localStorage.setItem('username', e.target[0].value)
+        setRedirect(true);
     }
-    return( 
-        <div className="home-component">
-            <form onSubmit={setUsernameSumbit}>
-                <label htmlFor="">
-                    Username: <input type="text"/>
-                    <button type="submit">Start</button>
-                </label>
-            </form>
-        </div>
-    );
+    if(redirect){
+        return(<Redirect to='/chat' />)
+    }else{
+        return( 
+            <div className="home-component">
+                <form onSubmit={setUsernameSumbit}>
+                    <label htmlFor="">
+                        Username: <input type="text"/>
+                        <button type="submit">Start</button>
+                    </label>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Home;
